ee8f8b26c726b8e97d4fac334b435e9075860a1b
abhibs <abhirambs97@gmail.com>
2025-03-06 17:10:33 +0530

    project created

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function userRegister()
+    {
+        return view('user.register');
+    }
+    public function userLogin()
+    {
+        return view('user.login');
+    }
+    public function userDashboard()
+    {
+        return view('user.dashboard');
+    }
+}
