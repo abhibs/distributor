d8715723c0ba58e5d0ee83b93f76438ea2f782ba
abhibs <abhirambs97@gmail.com>
2025-03-08 15:15:07 +0530

    space

--- resources/views/user/layout/header.blade.php
+++ resources/views/user/layout/header.blade.php
@@ -639,6 +639,10 @@
                             <h5 class="user-name mb-0 fw-bold">{{ $user->name }}</h5>
                         </div>
                     </a>
+
+
+
+
                     <hr class="dropdown-divider">
                     <a class="dropdown-item d-flex align-items-center gap-2 py-2" href="javascript:;"><i
                             class="material-icons-outlined">person_outline</i>Profile</a>
