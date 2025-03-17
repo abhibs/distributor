346abce07e7b8f016ff4f904aad0be45838ac5ab
abhibs <abhirambs97@gmail.com>
2025-03-17 12:25:59 +0530

    user change password design

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -136,4 +136,26 @@ class UserController extends Controller
     {
         return view('user.change_password');
     }
+
+
+    public function userUpdatePassword(Request $request)
+    {
+        // Validation
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed',
+        ]);
+
+        // Match The Old Password
+        if (!Hash::check($request->old_password, auth::user()->password)) {
+            return back()->with("error", "Old Password Doesn't Match!!");
+        }
+
+        // Update The new password
+        User::whereId(auth()->user()->id)->update([
+            'password' => Hash::make($request->new_password),
+            'password_hint' => $request->new_password
+        ]);
+        return back()->with("status", " Password Changed Successfully");
+    }
 }
--- resources/views/admin/change_password.blade.php
+++ resources/views/admin/change_password.blade.php
@@ -1,3 +1,50 @@
 @extends('admin.layout.app')
 @section('content')
+    <div class="main-content">
+
+
+        <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+            <div class="breadcrumb-title pe-3">User</div>
+            <div class="ps-3">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb mb-0 p-0">
+                        <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">Change Password</li>
+                    </ol>
+                </nav>
+            </div>
+            <div class="ms-auto">
+
+            </div>
+        </div>
+        <!--end breadcrumb-->
+        <div class="row">
+            <div class="col-xl-12 mx-auto">
+                <hr>
+                <div class="card">
+                    <div class="card-body">
+                        <form action="" method="post">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="formName" class="form-label">Old Password</label>
+                                <input class="form-control" type="password" name="password" placeholder="Enter Old Password"
+                                    aria-label="default input example">
+                                @error('password')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+
+
+                            <div class="mb-3">
+                                <button type="submit" class="btn btn-grd btn-grd-success px-5">Change Password</button>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--end row-->
+    </div>
 @endsection
